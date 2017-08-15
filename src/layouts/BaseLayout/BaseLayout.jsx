11d275fec100c89c87ac97b3b8e89fd546fd7f56
Maarten Brouwers <github@murb.nl>
2017-08-15 16:02:02 +0000

    added header html

--- src/layouts/BaseLayout/BaseLayout.jsx
+++ src/layouts/BaseLayout/BaseLayout.jsx
@@ -8,14 +8,31 @@ import { Route, Link } from 'react-router-dom';
 class BaseLayout extends Component {
   render () {
     return (
-      <div className='container text-center'>
-        <h1>KNMI React Redux Starter Kit</h1>
-        <Link to='/'>Home</Link>
-        {' · '}
-        <Link to='/counter'>Counter</Link>
-        <div className='page-layout__viewport'>
-          <Route exact path="/" component={MainComponent}/>
-          <Route path="/counter" component={CounterComponent}/>
+      <div>
+        <header className="row">
+          <div className="large-12 columns">
+            <img src="/logo-nl.png" className="logo" />
+            <ul className="user-area menu">
+              <li><a href="/inloggen"><span>Log in</span></a></li>
+            </ul>
+          </div>
+          <div className="top-bar">
+            <div className="menu-text"><span>KNMI Project</span></div>
+            <nav>
+              <ul className="menu">
+                <li><Link to='/' className="active">Home</Link></li>
+                <li><Link to='/counter'>Counter</Link></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <div className="row columns">
+          <h1>KNMI React Redux Starter Kit</h1>
+
+          <div className='page-layout__viewport'>
+            <Route exact path="/" component={MainComponent}/>
+            <Route path="/counter" component={CounterComponent}/>
+          </div>
         </div>
       </div>
     );
